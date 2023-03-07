f0004207968beed19968dda63bb489626baf0d59
dj3rry <davis.jerryjr@gmail.com>
2023-03-06 20:05:01 -0500

    added controllers

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();;
+const userRoutes = require('./user');
+const postRoutes = require('./post');
+const commentRoutes = require('./comment');
+
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
+
+module.exports = router;
